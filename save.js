810e623805142c35c3c39e2ea6b9704085060363
Paras700 <52033252+Paras700@users.noreply.github.com>
2019-06-20 19:04:02 +0530

    Add files via upload

---
+++ save.js
@@ -0,0 +1,17 @@
+function saveValue(){
+alert("call it");
+let name  = document.getElementsByName('Name');
+let user   = name[0].value;
+
+let des = document.getElementById('des').value; 
+
+let num = document.getElementById('num').value;
+
+let grade = document.getElementsByName('grade');
+
+let gra   = grade[0].value;
+
+let year = document.getElementsByName('year');
+let year_new = year[0].value;
+
+}
