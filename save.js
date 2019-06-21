462ef58f659540102c35ec4a441365bb2d103014
Paras700 <52033252+Paras700@users.noreply.github.com>
2019-06-21 19:23:22 +0530

    21/june  6:30 updated
    1.create localstorage with array form.
    2.show data in table form.
    3.click save button show thank you page.

--- save.js
+++ save.js
@@ -1,17 +1,4 @@
-function saveValue(){
-alert("call it");
-let name  = document.getElementsByName('Name');
-let user   = name[0].value;
+function fecthRecord(){
 
-let des = document.getElementById('des').value; 
-
-let num = document.getElementById('num').value;
-
-let grade = document.getElementsByName('grade');
-
-let gra   = grade[0].value;
-
-let year = document.getElementsByName('year');
-let year_new = year[0].value;
 
 }
